23c3a6c6f05fc053f7ec32351030ce9e642d6328
Lucsducks <lucas1617y@gmail.com>
2025-05-23 00:34:15 -0500

    added link izifile

--- src/app/(home)/components/projects/Project.tsx
+++ src/app/(home)/components/projects/Project.tsx
@@ -9,6 +9,8 @@ export default function Project() {
       icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>',
       color: "#4F46E5",
       order: 1,
+      buttonText: "Descargar para Android o Windows",
+      link: "https://izifile.eduardolucas.lat/",
       animationDelay: "0s",
     },
     {
